da2e2ada8ce9339aaf49635847e3fc33368585ac
cHaiitu Karanam <karanamchaitanyapramod@gmail.com>
2021-06-28 14:11:34 +0530

    heroku fixes

--- rr/model/address.js
+++
@@ -1,9 +0,0 @@
-class address{
-    constructor(addressid,formattedAddress,geometry){
-        this.addressid=addressid
-        this.formattedAddress=formattedAddress,
-        this.geometry=geometry
-    }
-}
-
-module.exports=address
--- rr/model/messages.js
+++
@@ -1,6 +0,0 @@
-export class messages{
-    constructor(obj){
-        this.messageType=obj.messageType,
-        this.message=obj.message
-    }
-}
